ea2e43602cc34e29e68356488446ec09be5bb464
Ferran Jovell <ferran@jovell.dev>
2025-01-25 11:00:40 +0100

    [nvim] Add an extra v to nearest test runner

--- nvim/lua/plugins/test.lua
+++ nvim/lua/plugins/test.lua
@@ -5,7 +5,7 @@ return {
         vim.g["test#strategy"] = "vimux"
         vim.g["test#python#runner"] = "pytest"
         vim.g["test#python#pytest#options"] = {
-            nearest = "--color yes -lsxv",
+            nearest = "--color yes -lsxvv",
             file = "--color yes --durations=10 -qk ''",
             suite = "--color yes --durations=10 -qk ''",
         }
