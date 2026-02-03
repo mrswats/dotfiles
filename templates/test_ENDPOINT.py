944d67473edda843dbfa382db77f76d21333fc6d
Ferran Jovell <ferran@jovell.dev>
2026-02-03 19:14:42 +0100

    [templates] Add more tests cases to test_ENDPOINT.py

--- templates/test_ENDPOINT.py
+++ templates/test_ENDPOINT.py
@@ -28,6 +28,18 @@ def test_ENDPOINT_status_code(list_ENDPOINT):
 
 
 @pytest.mark.django_db
+def test_ENDPOINT_unauthorized_status_code(list_ENDPOINT):
+    response = list_ENDPOINT()
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+
+
+@pytest.mark.django_db
 def test_ENDPOINT_data(list_ENDPOINT):
     response = list_ENDPOINT()
     assert response.json() == {}
+
+
+@pytest.mark.django_db
+def test_ENDPOINT_data_number_of_queries(list_ENDPOINT, django_assert_num_queries):
+    with django_assert_num_queries(0):
+        list_ENDPOINT()
